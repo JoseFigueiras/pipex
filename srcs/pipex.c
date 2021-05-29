c40479c2fb7167bb00eea231f02d48dde8d27a28
Josefig <josefiggaming@gmail.com>
2021-05-29 01:20:52 +0100

    lil bit of work

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,12 +1,27 @@
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	if (argc < 5)
-	{
-		printf("Need more args bro\n");
-		return (0);
-	}
+	//if (argc < 5)
+	//{
+	//	printf("Need more args bro\n");
+	//	return (0);
+	//}
 	
+	int		id;
+	int		argv_index;
+	char	**split;
+
+	id = fork();
+	if (id == 0)
+		run_command(argv, envp);
+	else
+		printf("Hi, I'm the parent process\n");
+	wait(NULL);
+	printf("Hello from both processes\n");
 	return (0);
 }
+
+
+	split = ft_split(argv[argv_index], ' ');
+	execve(split[0], split, envp);
